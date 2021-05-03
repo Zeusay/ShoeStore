ac1ee25a9f448244425e09e2fd90838c601b45d5
Stuart Kent <skent.dev@gmail.com>
2021-05-03 10:39:42 -0400

    Fix new shoes not being added to correct list

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -15,6 +16,8 @@ import kotlinx.android.synthetic.main.fragment_shoe_detail.*
 import timber.log.Timber
 
 class ShoeDetailFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,8 +47,6 @@ class ShoeDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         save_fab.setOnClickListener {
-            val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
-            findNavController().navigate(action)
             val newShoeName: String = shoeNameField.text.toString()
             val newShoeCompany: String = shoeCompanyField.text.toString()
             val newShoeDesc: String = shoeDescField.text.toString()
@@ -54,8 +55,10 @@ class ShoeDetailFragment : Fragment() {
             Timber.v("Shoe Info: $newShoeName, $newShoeCompany, $newShoeSize, $newShoeDesc")
 
             val shoeAdd : Shoe = Shoe(newShoeName, newShoeSize, newShoeCompany, newShoeDesc)
-            val viewModel: MainViewModel by viewModels()
             viewModel.addShoe(shoeAdd)
+
+            val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
+            findNavController().navigate(action)
         }
         return_fab.setOnClickListener {
             val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
